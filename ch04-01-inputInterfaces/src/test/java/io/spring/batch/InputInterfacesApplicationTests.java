e731ba3b84be8c8f29b883450d07aa03b76380c4
jbprek <jbprek@gmail.com>
2024-05-23 15:52:12 +0100

    WIP

---
+++ ch04-01-inputInterfaces/src/test/java/io/spring/batch/InputInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.batch;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class InputInterfacesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
